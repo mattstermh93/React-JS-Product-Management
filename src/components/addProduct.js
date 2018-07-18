a74b96d774efad57ece56382c96c19f80ddf73b5
mattstermh93 <mattstermh@hotmail.com>
2018-07-18 14:03:38 -0400

    added addProduct.js page

--- src/components/addProduct.js
+++ src/components/addProduct.js
@@ -3,8 +3,14 @@ import React, { Component } from 'react';
 class AddProduct extends Component {
   render() {
     return (
-      <div className="AddProduct">
-
+      <div className="AddProduct col-md-6">
+        <form>
+          <label>Product Name</label>
+          <input type="text" placeholder="Name..." ref="title" />
+          <label>Product Price:</label>
+          <input type="text" placeholder="Price..." ref="price" />
+          <input type="submit" value="Submit" />
+        </form>
       </div>
     );
   }
