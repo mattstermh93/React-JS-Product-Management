ec1173fa193742d3a842eb55f3db477c6e0a7400
mattstermh93 <mattstermh@hotmail.com>
2018-07-19 10:43:21 -0400

    added firebase.js

---
+++ src/firebase.js
@@ -0,0 +1,9 @@
+var config = {
+   apiKey: "AIzaSyDIiN3JeDI-Iw2otAHvAeX2DND--jPSpzw",
+   authDomain: "product-manager-7bfe1.firebaseapp.com",
+   databaseURL: "https://product-manager-7bfe1.firebaseio.com",
+   projectId: "product-manager-7bfe1",
+   storageBucket: "",
+   messagingSenderId: "174662676886"
+ };
+ firebase.initializeApp(config);
