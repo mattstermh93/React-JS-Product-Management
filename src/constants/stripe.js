c25c6ac32da01f1ca4446417571c7116cd1e38cf
Matthew Hasson <39917805+mattstermh93@users.noreply.github.com>
2018-07-20 12:01:19 -0400

    Delete stripe.js

--- src/constants/stripe.js
+++
@@ -1,5 +0,0 @@
-const STRIPE_PUBLISHABLE = process.env.NODE_ENV === 'production'
-  ? 'pk_live_Np989hTYSpc4G7j8zaudYzUY'
-  : 'pk_test_fkwFTBMg9zVbSUbUj1x7AtqA';
-
-export default STRIPE_PUBLISHABLE;
