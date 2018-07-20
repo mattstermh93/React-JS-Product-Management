c0cf50bcc6be6809da1b5023984be537222eef65
mattstermh93 <mattstermh@hotmail.com>
2018-07-20 10:17:54 -0400

    added payment failed and received functions in es6

--- src/checkout.js
+++ src/checkout.js
@@ -2,4 +2,19 @@ import React from 'react';
 import axios from 'axios';
 import StripeCheckout from 'react-stripe-checkout';
 
-import STRIPE_PUBLISHABLE from './constants/stripe'
+import STRIPE_PUBLISHABLE from './constants/stripe';
+import PAYMENT_SERVER_URL from './constants/server';
+
+const CURRENCY = 'USD'
+
+const fromUSDtoCent = amount => amount * 100;
+
+const successPayment = data => {
+  console.log(data);
+  alert('Payment was successful')
+}
+
+constant errorPayment = data => {
+  console.log(data);
+  alert('Payment error: did not work')
+}
