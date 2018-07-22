c54e6f5dd220eabf6969071308ff777d758c6d59
mattstermh93 <mattstermh@hotmail.com>
2018-07-22 15:02:35 -0400

    adjusted nav

--- src/components/header.js
+++ src/components/header.js
@@ -5,7 +5,7 @@ class Header extends Component {
   render() {
     return (
       <div className="Header">
-        <nav className="navbar navbar-expand-lg navbar-light bg-light">
+        <nav className="navbar navbar-expand-lg navbar-light bg-dark">
         <a className="navbar-brand" href="#">Product Management System</a>
         <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
           <span className="navbar-toggler-icon"></span>
